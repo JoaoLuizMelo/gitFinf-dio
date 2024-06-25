af74b868381f94efe1c2d841ecdbba002d0e7e08
JoaoLuizMelo <125713134+JoaoLuizMelo@users.noreply.github.com>
2024-06-25 05:38:21 -0300

    Done Header and web pack

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {HeaderCountainer , H1Header} from "./styles.js";
+
+
+export function Header() {
+  return (
+    <HeaderCountainer>
+      <H1Header>Git Find</H1Header>
+    </HeaderCountainer>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderCountainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #2d333b;
+  font-weight: 700;
+  height: 40px;
+`;
+
+export const H1Header = styled.h1`
+  color: blueviolet;
+  margin: 0;
+  padding: 0 100px;
+  font-size:24px;
+  color: #FFFFFF;
+
+`;
